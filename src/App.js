fb5200f9ce64496febeb15052653a46341104f49
bhanunori-txe <bhanu@tenxengage.com>
2025-06-05 16:20:47 +0530

    useState

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Header from './components/Header';
+import './App.css';
+import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
+import Footer from './components/Footer';
+
+
+function App() {
+    return (
+        <div className='App'>
+            <Header/>
+            <CampsitesDirectoryPage/>
+            <Footer/>
+        </div>
+    );
+}
+
+export default App;
