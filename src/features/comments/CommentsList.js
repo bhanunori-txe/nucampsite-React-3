933c0e3d4e2b1dea607c652e0e92ba07ed132b97
bhanunori-txe <bhanu@tenxengage.com>
2025-06-25 01:07:09 +0530

    week 3 workshop assignment

--- src/features/comments/CommentsList.js
+++ src/features/comments/CommentsList.js
@@ -1,26 +1,24 @@
 import { Col } from 'reactstrap';
 import Comment from './Comment';
-import {selectCommentsByCampsiteId}  from './commentsSlice';
+import { selectCommentsByCampsiteId } from './commentsSlice';
+import CommentForm from './CommentForm';
 
 const CommentsList = ({ campsiteId }) => {
     const comments = selectCommentsByCampsiteId(campsiteId);
 
-    if (comments && comments.length > 0) {
-        return (
-            <Col md='5' className='m-1'>
-                <h4>Comments</h4>
-                {comments.map((comment) => {
-                    return <Comment key={comment.id} comment={comment} />
-                })}
-            </Col>
-        );
-    }
-
     return (
         <Col md='5' className='m-1'>
-            There are no comments for this campsite yet.
+            <h4>Comments</h4>
+            {comments && comments.length > 0 ? (
+                comments.map(comment => (
+                    <Comment key={comment.id} comment={comment} />
+                ))
+            ) : (
+                <p>There are no comments for this campsite yet.</p>
+            )}
+            <CommentForm campsiteId={campsiteId} />
         </Col>
-    )
+    );
 };
 
 export default CommentsList;
