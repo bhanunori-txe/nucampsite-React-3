400f8366c53bb1188b4e8b4a392d554e36d43345
bhanunori-txe <bhanu@tenxengage.com>
2025-06-25 17:01:44 +0530

    Redux for All

--- src/features/comments/commentsSlice.js
+++ src/features/comments/commentsSlice.js
@@ -1,7 +1,22 @@
 // This file will not find any components
 //Use slice file will organize the logic for comments data
-
+import { createSlice } from '@reduxjs/toolkit';
 import { COMMENTS } from '../../app/shared/COMMENTS';
-export const selectCommentsByCampsiteId = (campsiteId) => {
-    return COMMENTS.filter((comment) => comment.campsiteId === parseInt(campsiteId))
-}
+
+const initialState = {
+    commentsArray: COMMENTS
+};
+
+const commentsSlice = createSlice({
+    name: 'comments',
+initialState
+});
+
+export const commentsReducer = commentsSlice.reducer;
+
+
+export const selectCommentsByCampsiteId = (campsiteId) => (state) => {
+    return state.comments.commentsArray.filter(
+        (comment) => comment.campsiteId === parseInt(campsiteId)
+    );
+};
