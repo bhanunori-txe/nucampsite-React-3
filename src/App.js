b8d52119fd64ac8ee5fbd8ee541b5e98b4017f39
bhanunori-txe <bhanu@tenxengage.com>
2025-07-01 04:35:22 +0530

    Async Partners

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import CampsiteDetailPage from './pages/CampsiteDetailPage';
 import Footer from './components/Footer';
 import AboutPage from './pages/AboutPage';
 import { fetchCampsites } from './features/campsites/campsitesSlice';
+import { fetchPartners } from './features/partners/partnersSlice';
 import './App.css';
 
 
@@ -18,6 +19,7 @@ function App() {
 
     useEffect(() => {
         dispatch(fetchCampsites());
+        dispatch(fetchPartners());
     }, [dispatch]);
 
     return (
